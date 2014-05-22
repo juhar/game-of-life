2c935b7a2a680a63bdd6f1af8b14b63157ad7872
Juha <juha.ruohonen@gmail.com>
2014-05-22 15:36:08 +0300

    Reverse last changes

--- gameoflife-core/src/main/java/com/wakaleo/gameoflife/domain/Cell.java
+++ gameoflife-core/src/main/java/com/wakaleo/gameoflife/domain/Cell.java
@@ -1,9 +1,9 @@
 package com.wakaleo.gameoflife.domain;
 
 public enum Cell {
-    LIVE_CELL("*"), DEAD_CELL(".");
+    LIVE_CELL("+"), DEAD_CELL(".");
 
-    private String symbol_break;
+    private String symbol;
 
     private Cell(final String initialSymbol) {
         this.symbol = initialSymbol;
